c0ce39211d9dc7a29444148cf32e1bfaec87f26f
shendapeng <shendapeng@yuantutech.com>
2016-09-11 11:01:43 +0800

    “”

--- view/libs/web-config.js
+++ view/libs/web-config.js
@@ -1,10 +1,9 @@
-let API_ROOT = 'http://fbi.yuantutech.com:3100';
-let MOCK_ROOT = 'http://fbi.yuantutech.com:3100/mock/';
+let API_ROOT = 'http://127.0.0.1:3100';
+let MOCK_ROOT = 'http://127.0.0.1:3100/mock/';
 
-console.log("===>",window.location);
-if (window.location.hostname.includes(".yuantutech.com")) {
-    API_ROOT = 'http://127.0.0.1:3100';
-    MOCK_ROOT = 'http://127.0.0.1:3100/mock/';
+if (window.location.hostname == "fbi.yuantutech.com") {
+    API_ROOT = 'http://fbi.yuantutech.com:3100';
+    MOCK_ROOT = 'http://fbi.yuantutech.com:3100/mock/';
 }
 
 module.exports = {
